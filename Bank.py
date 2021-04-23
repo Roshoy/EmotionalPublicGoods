124244f348e7829c000ae4cde714ad77b9ec1d68
krowka <lukaszkrowka98@gmail.com>
2021-04-23 09:59:25 +0200

    simple plots

--- Bank.py
+++ Bank.py
@@ -1,6 +1,3 @@
-from Agent import Agent
-
-
 class Bank:
     def __init__(self, factor):
         self.deposit = 0
@@ -9,11 +6,17 @@ class Bank:
     def multiply_deposit(self):
         self.deposit *= self.multiplication_factor
 
-    def pay_in(self, agent: Agent):
-        self.deposit += agent.contribute()
+    def pay_in(self, agents):
+        total_contribution = 0.0
+        for agent in agents:
+            agent_contribution = agent.contribute()
+            self.deposit += agent_contribution
+            total_contribution += agent_contribution
+        return total_contribution / len(agents)  # average contirbution
 
     def pay_out(self, agents):
         payoff = self.deposit / len(agents)
         for agent in agents:
             agent.receive(payoff)
         self.deposit = 0
+        return payoff
--- Main.py
+++ Main.py
@@ -1,14 +1,21 @@
 import random
+import matplotlib.pyplot as plt
 from Bank import Bank
 from Agent import Agent
 from Strategies import Greedy, Cooperative
 
+avg_contributions = []
+iterations = []
+payoffs = []
 
-def iteration(bank, agents):
-    for agent in agents:
-        bank.pay_in(agent)
+
+def iteration(bank, agents, i):
+    iterations.append(i)
+    avg_contribution = bank.pay_in(agents)
+    avg_contributions.append(avg_contribution)
     bank.multiply_deposit()
-    bank.pay_out(agents)
+    payoff = bank.pay_out(agents)
+    payoffs.append(payoff)
     # for agent in agents:
     #     print(agent)
 
@@ -19,8 +26,8 @@ agents = []
 greedy_strategy = Greedy()
 cooperative_strategy = Cooperative()
 
-free_riders_num = 10
-agents_num = 100
+free_riders_num = 1
+agents_num = 10
 
 for i in range(0, free_riders_num):
     agents.append(Agent(f'GreedyAgent{i}', greedy_strategy))
@@ -30,7 +37,14 @@ for i in range(free_riders_num, agents_num):
 
 for i in range(0, 10):
     print('Iteration', i)
-    iteration(bank, agents)
+    iteration(bank, agents, i)
 
 for agent in agents:
     print(agent)
+
+print(avg_contributions)
+print(payoffs)
+
+plt.plot(iterations, payoffs, iterations, avg_contributions)
+plt.ylabel('some numbers')
+plt.show()
